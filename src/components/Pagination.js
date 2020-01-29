f0964a7058e87709e123ee1a66fa1ce62f54610b
Kevin Chiu <keavenchiu@gmail.com>
2020-01-29 14:29:53 -0500

    styled pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,13 +7,29 @@ const ButtonContainer = styled.div`
     padding: 30px;
 `
 const PaginationText = styled.div`
+    display: flex;
+    align-items: center;
     padding-left: 15px;
     padding-right: 15px;
 `
 
 const NextButton = styled.button`
+    font-size: 20px;
+    padding: 5px 15px;
+    border-radius: 4px;
+
+    :hover {
+        background: #3092E8;
+    }
 `
 const PreviousButton = styled.button`
+    font-size: 20px;
+    padding: 5px 15px;
+    border-radius: 4px;
+
+    :hover {
+        background: #3092E8;
+    }
 `
 
 const Pagination = (props) => {
