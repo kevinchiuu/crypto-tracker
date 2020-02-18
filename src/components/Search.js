ce5a64161d0dc483ab1430eb7627a3c793a28753
Kevin Chiu <keavenchiu@gmail.com>
2020-02-18 16:42:23 -0500

    updated search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,35 +2,39 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const SearchContainer = styled.div`
-
 `
 
 class Search extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            searchQuery: ""
-        }
+            search: ""
+        };
 
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        console.log(event.target.value);
-
         const valueName = event.target.value;
 
-        this.setState({ searchQuery: valueName });
+        this.setState({ search: valueName });
+
+        console.log(this.state.search)
     }
 
-    render () {
+    render() {
         return (
             <SearchContainer>
                 <form> 
-                    <input onChange={this.handleChange} />
+                    <input 
+                    onChange={this.handleChange} 
+                    value={this.state.search}
+                    type="text"
+                    placeholder = "search"
+                    />
                 </form>
             </SearchContainer>
-        )
+        )   
     }
 }
 
